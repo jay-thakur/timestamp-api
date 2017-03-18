19f332a619033c5bfc6296b5c27ec77a0f1d8c62
Jay Prakash Thakur <jayprakashthakursnr@gmail.com>
2017-03-18 19:20:52 +0000

    api working commit

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const moment = require('moment');
+
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/:dateString', function(req, res){
+    const formats = [
+        'X',
+        'MMMM D, YYYY',
+        'MMMM D YYYY',
+        'MMM D, YYYY',
+        'MMM D YYYY',
+        'D MMMM YYYY',
+        'D MMMM, YYYY',
+        'D MMM YYYY',
+    ];
+    const date = moment(req.params.dateString, formats, true);
+    if(date.isValid()) {
+        res.json({
+            unix: date.format("X"),
+            natural: date.format("MMMM DD, YYYY")
+        });
+    } else {
+        res.json({
+            unix: null,
+            natural: null
+        });
+    }
 });
 
 app.listen(8080, function () {
-  console.log('timestamp app listening on port 8080!')
+  console.log('timestamp app listening on port 8080!');
 });
